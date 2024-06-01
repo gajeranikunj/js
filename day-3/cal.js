a68284cde7b86864bf9eebc36eb44b29c7046f1d
gajeranikunj <153923852+gajeranikunj@users.noreply.github.com>
2024-06-01 22:12:55 +0530

    add subole update

--- day-3/cal.js
+++ day-3/cal.js
@@ -18,15 +18,18 @@ document.addEventListener("keydown", (event) => {
 function data(e, sva) {
 
 
-    if (e.key[0]=="F") {
-      return;  
-    } 
+    if (e.key[0] == "F") {
+        return;
+    }
+
     var input;
+
     if (sva == undefined) {
         input = e.key;
     } else {
         input = sva;
     }
+
     var validInput = /^[0-9+\-*/.]*$/.test(input);
     if (!validInput) {
         input = input.replace(/[^0-9+\-*/.]/g, "");
@@ -38,13 +41,31 @@ function data(e, sva) {
     var arr = document.getElementById("enternum").value;
     var copy = arr.split("");
     var subl = "";
+    var tr = true;
+    var nowcopy = "";
+    copy.forEach((sub, index) => {
+        if (sub == "1" || sub == "2" || sub == "3" || sub == "4" || sub == "5" || sub == "6" || sub == "7" || sub == "8" || sub == "9" || sub == "0" || sub == ".") {
+            nowcopy += sub;
+            tr = true;
+        } else {
+            if (tr == true && index != 0) {
+                nowcopy += sub;
+                tr = false;
+            }
+        }
+    });
+
+    copy = nowcopy.split("");
+    document.getElementById("enternum").value = nowcopy;
+
+
     copy.forEach((sub) => {
         if (sub == "+" || sub == "-" || sub == "*" || sub == "/") {
             subl = subl + sub;
         }
     })
+ 
     subl = subl.split("")
-
     arr = arr.split(/[\s+\s-\s*\s/]+/);
     for (var tam in arr) {
         arr[tam] = arr[tam] * 1;
